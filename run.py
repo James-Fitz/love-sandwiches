b07df3a36ba5105b35c6271b4193749261de8b70
James Fitzpatrick <james.fitz123@gmail.com>
2022-12-14 13:47:39 +0000

    Add user request loop to repeat request for data until valid data is given

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -12,17 +13,25 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
+
 def get_sales_data():
     """
     Get sales input figures from the user
     """
-    print("\nPlease input sales data from last market day.")
-    print("Data should be 6 numbers separated by commas.")
-    print("Example: 22,31,17,24,20,29\n")
+    while True:
+        print("\nPlease input sales data from last market day.")
+        print("Data should be 6 numbers separated by commas.")
+        print("Example: 22,31,17,24,20,29\n")
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
+        validate_data(sales_data)
+        
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+        
+        return sales_data
 
 
 def validate_data(values):
@@ -39,5 +48,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+        
+    return True
 
-get_sales_data()
+
+data = get_sales_data()
